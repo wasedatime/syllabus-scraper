726c1eef93db432875ba9fe5656787c123b984cf
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2020-10-11 15:54:37 +0900

    add utils for uploading to s3

---
+++ syllabus/s3.py
@@ -0,0 +1,31 @@
+import boto3
+import os
+import json
+
+
+def upload_to_s3(syllabus: dict, dept: str):
+    """
+    Upload the syllabus info of the department to s3
+    :param syllabus: dictionary representation of the syllabus info
+    :param dept: abbr of the department. e.g. "PSE"
+    :return: dict :=
+    {
+        'Expiration': 'string',
+        'ETag': 'string',
+        'ServerSideEncryption': 'AES256'|'aws:kms',
+        'VersionId': 'string',
+        'SSECustomerAlgorithm': 'string',
+        'SSECustomerKeyMD5': 'string',
+        'SSEKMSKeyId': 'string',
+        'SSEKMSEncryptionContext': 'string',
+        'RequestCharged': 'requester'
+    }
+    """
+    s3 = boto3.resource('s3', region_name="ap-northeast-1")
+    syllabus_object = s3.Object(os.getenv('BUCKET_NAME'), os.getenv('OBJECT_PATH') + dept + '.json')
+    resp = syllabus_object.put(
+        ACL='public-read',
+        Body=bytes(json.dumps(syllabus).encode('UTF-8')),
+        ContentType='application/json'
+    )
+    return resp
