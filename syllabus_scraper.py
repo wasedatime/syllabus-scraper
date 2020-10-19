6b71bcc117533165340971366fab876654b1c37e
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2020-10-19 18:49:47 +0900

    clean test codes

--- syllabus_scraper.py
+++ syllabus_scraper.py
@@ -12,10 +12,3 @@ def handler(event, context):
     :return:
     """
     return None
-
-
-if __name__ == '__main__':
-    start = time.time()
-    cjl = SyllabusCrawler(engine="hybrid", dept='CJL', worker=32)
-    print(list(cjl.execute()))
-    print(time.time() - start)
